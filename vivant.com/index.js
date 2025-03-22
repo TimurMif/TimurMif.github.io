8a5392782f049b9d69e50fa07ae9b165f49864bc
TimurMif <162575403+TimurMif@users.noreply.github.com>
2025-03-22 19:58:02 +0400

    Add files via upload
    Add files for site

---
+++ vivant.com/index.js
@@ -0,0 +1,10 @@
+const header = document.querySelector('.background-grad');
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 0) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
+
+
